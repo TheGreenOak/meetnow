c4fdf4cf25947d7dc36f55b4a67b977f1ed83cbc
Yehonatan Rosenblatt <yeohnatan@gmail.com>
2021-12-14 23:27:24 +0200

    Major improvements to the Signaling tester

--- test/signaling.py
+++ test/signaling.py
@@ -1,5 +1,7 @@
 from tester import Connection
 
+import threading
+
 
 DEFAULT_SPORT = 13377
 SIGNALING_PORT = 5060
@@ -12,6 +14,9 @@ SWITCH = 3
 LEAVE = 4
 END = 5
 
+ID_INDEX = 0
+PASSWORD_INDEX = 1
+
 
 def print_menu():
     print("1 - Start")
@@ -22,6 +27,21 @@ def print_menu():
     print()
 
 
+def recv_continously(conn):
+    try:
+        while conn.keep_running:
+            try:
+                data = conn.recv(1024)
+                if not data: # If the server has closed the connection, we'll get empty data
+                    break
+
+                print("\n[SERVER]", data)
+            except BlockingIOError:
+                pass
+    except:
+        print("Server aborted connection.")
+
+
 def main():
     print("Welcome to the basic server tester.")
 
@@ -30,23 +50,32 @@ def main():
     if ip == "1": ip = "localhost"
     print()
 
-    conn = Connection(ip, DEFAULT_SPORT, SIGNALING_PORT, CONN_TYPE)
+    print("Please enter the port number you'd like to connect from (press 1 for port 13377): ", end="")
+    sport = int(input(""))
+    if sport == 1: sport = DEFAULT_SPORT
+    print()
+
+    conn = Connection(ip, sport, SIGNALING_PORT, CONN_TYPE)
 
     try:
         conn.connect()
+        conn.toggle_blocking_mode()
+
+        # We make a new thread for receiving data from the server without interrupting the main thread
+        recv_thread = threading.Thread(target=recv_continously, args=(conn,))
+        recv_thread.start()
+
         print("Connection established.")
         print_menu()
 
         while True:
-            print("Select an option: ", end="")
             option = int(input(""))
 
             if option < 1 or option > 5:
                 print("Invalid option.")
             else:
-                if option == START:
-                    conn.send('{"request": "start"}')
-                    print("Response: ", conn.recv(RECV_SIZE))
+                if option == START: conn.send('{"request": "start"}')
+
                 elif option == JOIN:
                     print("Enter meeting ID: ", end="")
                     meeting_id = input("")
@@ -55,16 +84,10 @@ def main():
                     password = input("")
 
                     conn.send('{{"request": "join", "id":"{}", "password":"{}"}}'.format(meeting_id, password))
-                    print("Response: ", conn.recv(RECV_SIZE))
-                elif option == SWITCH:
-                    conn.send('{"request": "switch"}')
-                    print("Response: ", conn.recv(RECV_SIZE))
-                elif option == LEAVE:
-                    conn.send('{"request": "leave"}')
-                    print("Response: ", conn.recv(RECV_SIZE))
-                elif option == END:
-                    conn.send('{"request": "end"}')
-                    print("Response: ", conn.recv(RECV_SIZE))
+                    
+                elif option == SWITCH: conn.send('{"request": "switch"}')
+                elif option == LEAVE: conn.send('{"request": "leave"}')
+                elif option == END: conn.send('{"request": "end"}')
     except KeyboardInterrupt:
         print("Stopping...")
     except TimeoutError:
@@ -72,6 +95,7 @@ def main():
     except:
         print("An unknown error occurred.")
     finally:
+        conn.keep_running = False
         conn.close()
         print("Connection closed.")
 
