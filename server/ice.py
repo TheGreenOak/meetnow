d25fd05ea69d388e8d0e467b44a98819346823e0
Yehonatan Rosenblatt <yeohnatan@gmail.com>
2022-01-05 06:53:59 +0200

    WIP: Added RedisDB

--- server/ice.py
+++ server/ice.py
@@ -1,4 +1,6 @@
 from server import TCPServer
+from database.redisdb import RedisDictDB
+from threading import Thread, Event
 
 
 # Constants
@@ -13,16 +15,31 @@ class ICE(TCPServer):
     will be able communicate with each other by synchronizing communication ports and IP addresses.
     """
 
-    def __init__(self, port):
+    def __init__(self, port, public_db):
         super().__init__(port)
+
+        # Setup the databases needed
+        self.users = {}
+        self.public_db = public_db
+
+        self.expiry_stopper = Event()
 
 
 def main():
-    server = ICE(SERVER_PORT)
+    # Connect to the public database
+    try:
+        public_db = RedisDictDB("meetings")
+    except RuntimeError:
+        print("Could not connect to the Redis Database.")
+        print("Please make sure that Redis is running on the local machine with the default parameters.")
+        exit(1)
+
+    server = ICE(SERVER_PORT, public_db)
     server.start()
+    server.toggle_blocking_mode()
     
     print("Signaling server started")
-    # TODO: server.run() 
+    server.run() # Blocking method - will continue running until the admin presses Ctrl+C
 
     print("\nStopping server...")
     server.stop()
