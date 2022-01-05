a15291011113691f774f464f96e8456dcde6a7f2
Alon Green <alongreenalgr@gmail.com>
2022-01-05 09:46:30 +0200

    Updated redisby from backend/signaling

---
+++ server/database/redisdb.py
@@ -0,0 +1,37 @@
+import redis
+
+
+class RedisDictDB:
+    def __init__(self, prefix=None):
+        self.prefix = prefix
+
+        try:
+            self.db = redis.Redis(decode_responses=True)
+            self.db.ping()
+        except redis.ConnectionError:
+            raise RuntimeError("Could not connect to Redis server")
+    
+
+    def __getitem__(self, key):
+        """
+        Implementation for the [] operator.
+        """
+        retVal = self.db.hgetall(f"{self.prefix if self.prefix else ''}{':' if self.prefix else ''}{key}")
+
+        if retVal:
+            return retVal
+        return None
+    
+
+    def __setitem__(self, key, value):
+        """
+        Implementation for the [] = operator.
+        """
+        self.db.hmset(f"{self.prefix if self.prefix else ''}{':' if self.prefix else ''}{key}", value)
+    
+
+    def __delitem__(self, key):
+        """
+        Implementation for the del operator.
+        """
+        self.db.delete(f"{self.prefix if self.prefix else ''}{':' if self.prefix else ''}{key}")
