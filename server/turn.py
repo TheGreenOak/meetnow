4279e27d7e075ec027bc31bc0e6b8079fe6ff633
Alon Green <alongreenalgr@gmail.com>
2022-01-08 19:58:46 +0200

    Implemented the HEARTBEAT protocol
    TODO: fix the bug where the server prints that the user is disconnected even though its not

--- server/turn.py
+++ server/turn.py
@@ -4,7 +4,8 @@ from json import loads as deserialize, dumps as serialize
 
  # TODO: 
 SERVER_PORT = 3479 # The TURN port except for the last number  - https://www.3cx.com/blog/voip-howto/stun-voip-1/
-
+MINUTE = 60
+USER_TTL_MESSAGES = 2
 
 
 class NoMeetingID(Exception):
@@ -33,36 +34,49 @@ class Turn(UDPServer):
     def __init__(self, port):
         super().__init__(port)
 
-        self.users = {} # {"user_uuid", (127.0.0.1, 13377)}
+        self.users = {
+                ("127.0.0.1", 1337) : {"ttl": 2, "peer" : ("128.0.0.1", 42069)},
+                ("128.0.0.1", 42069) : {"ttl": 2, "peer" : ("127.0.0.1", 1337)}
+            }
+
+        self.expiry_stopper = Event()
 
     def run(self):
+        threads = []
+
         try:
+            # Create expiration worker threads
+            expiry_workers = [Thread(target=self.user_cleaner)]
+            for worker in expiry_workers:
+                worker.start()
+                threads.append(worker)
+
             while True:
                 try:
                     # In UDP, there are no sockets. Therefore, we just need to receive data.
                     data, address = self.recv()
+                    self.users[(address[0], address[1])]["ttl"] = USER_TTL_MESSAGES # If we received a message from the user, they're alive.
                     data = data.decode()
                     if address not in self.connected_users:
                         self.connected_users.append(address)
                         
-                    print("[{}:{}] {}".format(address[0], address[1], data))
+                    print(f"[{address[0]}:{address[1]}] {data}")
 
                     try:
                         dest = self.check_meeting_valid(address, data)
                     except Exception as e:
+                        print(e)
                         self.return_exception(address, e)
                     
-                    self.forward(data, dest)
+                    self.forward(dest, data)
 
                 except BlockingIOError:
                     pass
         except KeyboardInterrupt:
             self.keep_running = False
 
-            print("\nStopping server...")
-            self.stop()
 
-    def check_meeting_valid(self):
+    def check_meeting_valid(self, address, data):
         if 1!=1: # the meeting id is not in the database
             raise NoMeetingID
         if 1!= 1: # if the password is incorrect
@@ -72,16 +86,69 @@ class Turn(UDPServer):
         if 1!=1: # check if the user is connected to the turn server
             raise OtherUserNotConnected
         # return participant to send to
+        return ("127.0.0.1", 1337)
     
     def return_exception(self, address, exception):
-        exception = "S" + exception
+        exception = "S" + exception 
+        exception.encode()
         self.send(address, exception)
 
     def forward(self, address, data):
         data = "U" + data
+        data = data.encode()
         self.send(address, data)
     
+    def user_cleaner(self):
+        """
+        This method is responsible for checking the user's TTL, and will send a heartbeat every minute.
+        If the user's TTL is 0, they will be disconnected.
+        """
 
+        # We want to run this worker until the server stops, at which point
+        # this event will get set.
+        while not self.expiry_stopper.is_set():
+            counter = 0
+
+            for user_uuid, user in self.users.copy().items():
+                if user["ttl"] <= 0: # The user has probably disconnected
+                    self.disconnect_client(user_uuid)
+                    counter += 1
+                else:
+                    user["ttl"] = user["ttl"] - 1
+                    self.send(user_uuid, b"HEARTBEAT")
+
+            if counter > 0:
+                print("[EXPIRY WORKER] Disconnected {} client{}".format(counter, 's' if counter > 1 else ''))
+
+            self.expiry_stopper.wait(MINUTE) # We put this function to "sleep" that can be awoken
+
+        del self.users[user_uuid]
+        print("[DISCONNECTED] {}:{}".format(user_uuid[0], user_uuid[1]))
+
+    def disconnect_client(self, address):
+        """
+        This method is responsible for removing a client from the database, and disconnecting their socket.
+
+        No checks are performed to ensure that the client is in the database.
+        Call this method only after checking that the client is in the database.
+
+        address (tuple): Address tuple or other unique identifier.
+
+        returns: tuple | None: The message that needs to be sent to the socket (both in tuple).
+        """
+
+        # Setup temporary variables
+        user = self.users[address]
+        second_user = None
+
+        # If the user disconnects in a meeting, remove them from the meeting
+        if user.get("id"):
+            second_user = self.disconnect_peer(address)
+        
+        del self.users[address]
+
+        if second_user:
+            return (second_user, serialize({"response": "info", "type": "left"}))
 
 
 def main():
