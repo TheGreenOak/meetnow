df69cd31f46cbed3e9bf048b2ee11773a2bf6c9d
Yehonatan Rosenblatt <yeohnatan@gmail.com>
2022-01-29 17:58:08 +0200

    Made simple Webcam on/off demo

---
+++ src/components/Webcam.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    let videoSource = null;
+    const videoStream = navigator.mediaDevices.getUserMedia({
+        video: true
+    });
+
+    videoStream.then((stream) => {
+        videoSource.srcObject = stream;
+        videoSource.play();
+    }).catch((err) => console.error(err));
+</script>
+
+<!-- svelte-ignore a11y-media-has-caption -->
+<video bind:this={videoSource} />
