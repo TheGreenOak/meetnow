6de3cd0869e0b5c79ba33a1c07716a45db03e44c
Yehonatan Rosenblatt <yeohnatan@gmail.com>
2022-01-29 13:01:41 +0200

    Updated Makefile to compile for JS

--- Makefile
+++ Makefile
@@ -9,9 +9,20 @@ OPENCV_FIX  = $$(pkg-config opencv4 --libs --cflags)
 OUT_DIR     = bin
 OUT_FILE    = build
 
+# JS Variables
+JS_COMPILER = emcc
+JS_FLAGS    = -s EXPORTED_FUNCTIONS=_encode,_decode -s EXPORTED_RUNTIME_METHODS=cwrap
+JS_INCLUDES = -Iinclude
+JS_SOURCE   = src/codec.cpp
+JS_OUT_FILE = codec.js
+
 build:
 	mkdir -p bin
 	${COMPILER} ${CPP_FLAGS} -o ${OUT_DIR}/${OUT_FILE} ${INCLUDES} ${OPENCV_FIX} ${SOURCE_DIR}
+
+js:
+	mkdir -p bin
+	${JS_COMPILER} ${JS_SOURCE} -o ${OUT_DIR}/${JS_OUT_FILE} ${JS_INCLUDES} ${JS_FLAGS}
 
 run:
 	./${OUT_DIR}/${OUT_FILE}
--- include/codec.h
+++ include/codec.h
@@ -2,5 +2,7 @@
 
 #define DEFAULT_THRESHOLD 20
 
-unsigned char* encode(unsigned char* pixels, unsigned char* previousFrame, unsigned short height, unsigned short width, unsigned char threshold = DEFAULT_THRESHOLD);
-unsigned char* decode(unsigned char* pixels, unsigned char* previousFrame, unsigned short height, unsigned short width);
+extern "C" {
+	unsigned char* encode(unsigned char* pixels, unsigned char* previousFrame, unsigned short height, unsigned short width, unsigned char threshold = DEFAULT_THRESHOLD);
+	unsigned char* decode(unsigned char* pixels, unsigned char* previousFrame, unsigned short height, unsigned short width);
+}
