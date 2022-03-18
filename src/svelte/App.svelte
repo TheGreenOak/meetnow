d14cd7e986bcbff198213c08291d57ba1764ba1f
Yehonatan Rosenblatt <yeohnatan@gmail.com>
2022-03-19 01:03:06 +0200

    Updated App and Welcome

--- src/svelte/App.svelte
+++ src/svelte/App.svelte
@@ -1,28 +1,62 @@
 <script lang="ts">
+	import { Writable, writable } from "svelte/store";
+	import type { Networking, SignalingState } from "../electron/backend";
+
 	import Header from "./components/Header.svelte";
 	import Welcome from "./components/Welcome.svelte";
 	import Meeting from "./components/Meeting.svelte";
 	
-    let inMeeting: boolean = false;
+	const net: Networking = (window as any).networking;
+	const meeting: Writable<MeetingInfo> = writable();
+
+	let id: string | undefined;
+	let password: string | undefined;
+	let inMeeting: boolean = false;
+
+	meeting.subscribe(info => {
+		id = info?.id;
+		password = info?.password;
+	});
+
+	// Handle networking
+	net.on("state-change", (state: SignalingState) => {
+		if (state.newState == "created") {
+			id = state.id;
+			password = state.password;
+
+			net.join(state.id, state.password);
+		}
+
+		else if (state.newState == "connected" && state.me) {
+			inMeeting = true;
+
+			meeting.set({
+				id: id,
+				password: password,
+				temporary: false
+			});
+		}
+
+		else if (state.newState == "ended" || (state.newState == "disconnected" && state.me)) {
+			meeting.set({ temporary: false });
+
+			inMeeting = false;
+		}
+	});
+
+	type MeetingInfo = {
+        id?: string;
+        password?: string;
+        temporary: boolean;
+    };
 </script>
 
 <main>
-	
-	<Header />
+	<Header {meeting} />
+
     {#if inMeeting}
         <Meeting />
     {:else}
-        <Welcome />
+        <Welcome {meeting} />
     {/if}
-
-
 </main>
-
-<style>
-	
-	:global(body) {
-		background-color: rgb(26 28 29);;
-	}
-
-
-</style>
--- src/svelte/components/Welcome.svelte
+++ src/svelte/components/Welcome.svelte
@@ -1,77 +1,38 @@
 <script lang="ts">
+    import type { Writable } from "svelte/store";
+    import type { Networking } from "../../electron/backend";
 
-    import type { Networking, SignalingState } from "../../electron/backend";
-
+    export let meeting: Writable<{}>;
     const net: Networking = (window as any).networking;
 
     let id: string | undefined;
-	let password: string | undefined;
-	let hide: string = "";
-	let ready: string = "ready";
-
-    net.on("state-change", (state: SignalingState) => {
-		if (state.newState == "created") {
-			id = state.id;
-			password = state.password;
-
-			net.join(state.id, state.password);
-		}
-
-		else if (state.newState == "connected" && state.me) {
-			console.log("Connected!");
-			console.log("ID: " + id);
-			console.log("Password: " + password);
-			hide = "hide";
-		}
-
-		else if (state.newState == "connected" && !state.me) {
-			console.log("A peer has connected!");
-		}
-    });
-
-    net.on("ready", () => {
-		ready = "ready";
-	});
-
-	net.on("comm-error", (err) => {
-		console.error("Unable to connect with peer B!");
-		console.error("ERR: " + err);
-	});
-
-	net.on("error", (err) => {
-		console.error(err);
-	});
-
-	net.on("message", (content) => {
-		console.log("PEER: " + content);
-	});
+    let password: string | undefined;
 
     function startMeeting() {
 		net.start();
 	}
 
-	function joinMeeting(e: Event) {
-		e.preventDefault();
+	function joinMeeting() {
+        meeting.set({
+            id: id,
+            password: password,
+            temporary: true
+        });
+        
 		net.join(id, password);
 	}
-
-
 </script>
 
-<button id="start-btn" class="{hide}" on:click={startMeeting}>Start Meeting</button>
+<button id="start-btn" on:click={startMeeting}>Start Meeting</button>
 
-<form id="meeting-form" class="pre-meeting {hide}" on:submit={joinMeeting}>
+<form id="meeting-form" class="pre-meeting" on:submit|preventDefault={joinMeeting}>
     <input type="text" id="meeting-id" name="meeting-id" placeholder="ID" bind:value={id} />
     <input type="password" id="meeting-password" name="meeting-password" placeholder="Password" bind:value={password} />
 </form>
 
-<button id="join-btn" class="{hide}" type="submit" value="Join" on:click={joinMeeting}>Join Meeting</button>
-
-
-
+<button id="join-btn" type="submit" value="Join" on:click={joinMeeting}>Join Meeting</button>
 
 <style>
-
     #start-btn {
         width:25%;
         margin-left:37.5%;
@@ -89,17 +50,4 @@
         margin-left:37.5%;
         margin-right:37.5%;
     }
-
-	button.pre-meeting {
-		background-color: aqua;
-		font-size: 16px;
-        width:50%;
-        margin-left:25%;
-        margin-right:25%;
-	}
-
-    .hide {
-		display: none;
-	}
-
 </style>
