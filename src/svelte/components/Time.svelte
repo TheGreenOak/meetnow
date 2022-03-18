7309528632278098a8a58f95991e58667f19cc86
Yehonatan Rosenblatt <yeohnatan@gmail.com>
2022-03-19 00:22:11 +0200

    Added a time component

---
+++ src/svelte/components/Time.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+
+    let time = new Date();
+
+    $: hour = time.getHours().toString().padStart(2, '0');
+    $: minute = time.getMinutes().toString().padStart(2, '0');
+
+    onMount(() => {
+        const interval = setInterval(() => {
+            time = new Date();
+        }, 1000);
+
+        // Called upon destroyal of the object
+        return () => { 
+            clearInterval(interval);
+        }
+    });
+</script>
+
+<h1>{hour}:{minute}</h1>
+
+<style>
+    h1 {
+        color: white;
+    }
+</style>
